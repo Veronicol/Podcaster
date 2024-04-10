3c07bf66785360d63365bb4e436958d04497e1c4
Veronicol <veronica.celemin@gmail.com>
2024-04-10 19:22:29 +0200

    fix: navbar navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+
 export const NavBar = () => {
+  const navigate = useNavigate();
+
+  const navigateToHome = () => navigate('/');
   return (
     <div className="navbar-container">
-      <a href="/">Podcaster</a>
+      <div onClick={navigateToHome}>Podcaster</div>
       <div>Loading...</div>
     </div>
   );
