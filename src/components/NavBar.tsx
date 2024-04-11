ec9c408487a575c36d1aa0a73ff5ccf1dc373ae0
Veronicol <veronica.celemin@gmail.com>
2024-04-11 11:39:52 +0200

    feat: add loading state

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,16 @@
+import { useIsFetching } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 export const NavBar = () => {
   const navigate = useNavigate();
+  const isFetching = useIsFetching();
 
   const navigateToHome = () => navigate('/');
+
   return (
     <div className="navbar-container">
       <div onClick={navigateToHome}>Podcaster</div>
-      <div>Loading...</div>
+      {isFetching > 0 && <div className="loading" />}
     </div>
   );
 };
