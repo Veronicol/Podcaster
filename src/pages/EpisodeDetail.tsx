fa628f1d71f9dd1a4b37ba4df830d71ae1e04a67
Veronicol <veronica.celemin@gmail.com>
2024-04-11 14:43:48 +0200

    feat: parse episode description

--- src/pages/EpisodeDetail.tsx
+++ src/pages/EpisodeDetail.tsx
@@ -1,3 +1,4 @@
+import parse from 'html-react-parser';
 import { useParams } from 'react-router-dom';
 
 import { PodcastResumeCard } from '../components';
@@ -8,6 +9,10 @@ export const EpisodeDetail = () => {
 
   const episodeDetail = useGetEpisodeDetail(podcastId || '', episodeId || '');
 
+  const parsedDescription = parse(
+    episodeDetail?.description || 'No description available'
+  );
+
   return (
     <>
       {episodeDetail && (
@@ -15,7 +20,7 @@ export const EpisodeDetail = () => {
           <PodcastResumeCard />
           <div className="episode-container box">
             <div className="title">{episodeDetail.title}</div>
-            <div className="description">{episodeDetail.description}</div>
+            <div className="description">{parsedDescription}</div>
             <audio controls src={episodeDetail.episodeUrl}></audio>
           </div>
         </div>
