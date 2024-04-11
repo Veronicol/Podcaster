7852d221fd058e3449bf5182d1597c08beba6058
Veronicol <veronica.celemin@gmail.com>
2024-04-11 10:36:06 +0200

    feat: add navigation links to resume card

--- src/components/PodcastResumeCard.tsx
+++ src/components/PodcastResumeCard.tsx
@@ -1,21 +1,37 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { useGetPodcastResume } from '../hooks';
 
 export const PodcastResumeCard = () => {
   const { podcastId, episodeId } = useParams();
-  const hasInfoAsLink = !episodeId;
+  const navigate = useNavigate();
+  const hasInfoAsLink = !!episodeId;
 
   const podcastResume = useGetPodcastResume(podcastId || '');
+
+  const navigateToDetail = () => {
+    hasInfoAsLink && navigate(`/podcast/${podcastId}`);
+  };
 
   return (
     <>
       {podcastResume && (
         <div className="card-resume-container box">
-          <img src={podcastResume.image} alt={podcastResume.image} />
-          <div className="main-info-container">
-            <div className="main-info">{podcastResume.name}</div>
-            <div className="secondary-info">by {podcastResume.author}</div>
+          <img
+            className={`${hasInfoAsLink ? 'clickable' : ''}`}
+            src={podcastResume.image}
+            alt={podcastResume.image}
+            onClick={navigateToDetail}
+          />
+          <div className="main-info-container" onClick={navigateToDetail}>
+            <div className={`main-info ${hasInfoAsLink ? 'clickable' : ''}`}>
+              {podcastResume.name}
+            </div>
+            <div
+              className={`secondary-info ${hasInfoAsLink ? 'clickable' : ''}`}
+            >
+              by {podcastResume.author}
+            </div>
           </div>
           <div className="content-info-container">
             <div className="title">Description:</div>
